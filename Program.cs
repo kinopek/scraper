9a2520731cf79d1dde5b52ac52c67b3f822d2969
kinopek <34396864+kinopek@users.noreply.github.com>
2021-05-11 10:16:25 +0200

    encapsulation, post, put, get

--- Program.cs
+++ Program.cs
@@ -7,7 +7,7 @@ using VisioForge.Shared.Newtonsoft.Json;
 
 namespace scraper
 {
-    class Walutka
+   /* class Walutka
     {
         public object time, bpi; 
         public Walutka(object t, object b)
@@ -15,7 +15,7 @@ namespace scraper
             time = t;
             bpi = b;
         }
-    }
+    }*/
     class Program
     {
         static async System.Threading.Tasks.Task Main(string[] args)
@@ -23,39 +23,40 @@ namespace scraper
 
             const string fromUrl = "https://api.coingecko.com/api/v3/exchange_rates";
             string intoUrl = "http://apinaszedocelowe.com.pl/szczecin/szanty/golebabki";
-            Walutka data;
+            //Walutka data;
+
+
+            bool exists = await CheckIfDatabaseExistsAsync(intoUrl);
 
             while (true)
             {
-                HttpWebRequest request = (HttpWebRequest)WebRequest.Create(fromUrl);
-                //request.Method = Get();
-                request.ContentType = "application/json";
-               // request.ContentLength = Data.Length;
+                
+
                 try
                 {
-                    WebResponse webResponse = request.GetResponse();
-                    Stream webStream = webResponse.GetResponseStream();
-                    StreamReader responseReader = new StreamReader(webStream);
-                    string response = responseReader.ReadToEnd();
+                    string response = ScrapFromCoinGecko(fromUrl);
+                   
                    object obiekt = JsonConvert.DeserializeObject<object>(response);
 
-                   //data = JsonConvert.DeserializeObject<Walutka>(response);
-                    
-                      //Console.WriteLine(data.time);
-                       //Console.WriteLine(data.bpi);
+                    //data = JsonConvert.DeserializeObject<Walutka>(response);
 
-                    responseReader.Close();
-                   
-                    var client = new HttpClient();
                     var values = new Dictionary<string, string>()
                     {
                         {"", obiekt.ToString()},
-                        //{"bpi", data.bpi.ToString()},
                     };
                     Console.WriteLine(obiekt.ToString());
-                    var content = new FormUrlEncodedContent(values);
-                    var responseSend = await client.PostAsync(intoUrl, content);
-                    responseSend.EnsureSuccessStatusCode();
+
+                    if (!exists)
+                    {
+                        CreateDatabaseAsync(values, intoUrl);
+                        exists = true;
+                    }
+                    else
+                    {
+                        UpdateDatabaseAsync(values, intoUrl);
+                    }
+
+                   
 
                 }
                 catch (Exception e)
@@ -66,6 +67,52 @@ namespace scraper
               
                 System.Threading.Thread.Sleep(1000);
             }
+        }
+
+        private static string ScrapFromCoinGecko(string fromUrl)
+        {
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(fromUrl);
+            request.ContentType = "application/json";
+            WebResponse webResponse = request.GetResponse();
+            Stream webStream = webResponse.GetResponseStream();
+            StreamReader responseReader = new StreamReader(webStream);
+            string response=  responseReader.ReadToEnd();
+            responseReader.Close();
+            return response;
+        }
+
+        private static async System.Threading.Tasks.Task UpdateDatabaseAsync(Dictionary<string, string> values, string intoUrl)
+        {
+            var client = new HttpClient();
+            var content = new FormUrlEncodedContent(values);
+            var responseSend = await client.PutAsync(intoUrl, content);
+            responseSend.EnsureSuccessStatusCode();
+        }
+
+        private static async System.Threading.Tasks.Task<bool> CheckIfDatabaseExistsAsync(string Url)
+        {
+            var client = new HttpClient();
+            var responseSend = await client.GetAsync(Url);
+            responseSend.EnsureSuccessStatusCode();
+
+            if (responseSend !=null)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+
+            }
+        }
+
+        private static async System.Threading.Tasks.Task CreateDatabaseAsync(Dictionary<string, string> values, string intoUrl)
+        {
+            var client = new HttpClient();
+            var content = new FormUrlEncodedContent(values);
+            var responseSend = await client.PostAsync(intoUrl, content);
+            responseSend.EnsureSuccessStatusCode();
+
         }
     }
 }
